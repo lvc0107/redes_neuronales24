bc39746e3fa39fc0edcbb80f3a4b621766c96946
Migue Vargas <lvc0107@proton.me>
2024-10-11 19:03:59 -0300

    Added first implementation

---
+++ trabajo_practico1.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+"""
+Created on Sun Aug 25 20:41:09 2024
+
+@author: luisvargas
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from ode import integrador_ode, rk4
+
+
+def alpha_n(v):
+    return 0.01 * (10.0 - v) / (np.exp((10.0 - v) / 10.0) - 1.0)
+
+
+def alpha_m(v):
+    return 0.1 * (25.0 - v) / (np.exp((25.0 - v) / 10.0) - 1.0)
+
+
+def alpha_h(v):
+    return 0.07 * np.exp(-v / 20.0)
+
+
+def beta_n(v):
+    return 0.125 * np.exp(-v / 80.0)
+
+
+def beta_m(v):
+    return 4.0 * np.exp(-v / 18.0)
+
+
+def beta_h(v):
+    return 1.0 / (np.exp((30.0 - v) / 10.0) + 1.0)
+
+
+def ina(x, p):
+    """
+    Ganancia de la corriente i por una canal de Sodio Na
+
+    Parameters
+    ----------
+    x : TYPE
+        DESCRIPTION. parametros para ?
+    p : TYPE
+        DESCRIPTION. parametros para?
+
+    Returns
+    -------
+    La ganancia de iones que pasan a traves de un canal de sodio
+
+     El Canal de Sodio tiene 3 compuertas de tipo m que se abren y dejan pasar la corriente
+     y una de tipo h que debe estar inactiva para que dejen pasar la corriente
+
+    """
+    v = x[0]  # potencial
+    m = x[2]  # probabilidad de que m este abierta
+    h = x[3]  # probabilidad de que h este inactiva
+    gna = p[1]  # gna
+    vna = p[4]  # vna
+    """
+    para que pase i deben habrirse las 3 compuertas m y la h debe estar inactiva
+    """
+    pna = m**3 * h
+    """
+    gna * (v- vna) : conductividad promedio o ganancia promedio
+    """
+    return gna * (v - vna) * pna
+
+
+def ik(x, p):
+    """
+
+    Ganancia de la corriente i por una canal de Potasio K
+
+    Parameters
+    ----------
+    x : TYPE
+        DESCRIPTION. parametros para probabilidades
+    p : TYPE
+        DESCRIPTION. parametros para formulas
+
+    Returns
+    -------
+    La ganancia de iones que pasan a traves de un canal de Potasio
+
+     El Canal de Sodio tiene 4 compuertas de tipo m que se abren y dejan pasar la corriente
+
+    """
+    v = x[0]
+    n = x[1]
+    gk = p[2]
+    vk = p[5]
+    """
+    para que pase i deben habrirse las 4 compuertas de tipo n al mismo tiempo
+    """
+    pk = n**4
+    return gk * (v - vk) * pk
+
+
+def il(x, p):
+    """
+
+    perdida de la corriente i
+
+    Parameters
+    ----------
+    x : TYPE
+        DESCRIPTION. parametros para probabilidades
+    p : TYPE
+        DESCRIPTION. parametros para formulas
+
+
+    Returns
+    -------
+        TODO completar
+
+    """
+    v = x[0]
+    gl = p[3]
+    vl = p[6]
+    return gl * (v - vl)
+
+
+# correspondientes tiempos caracteristicos y valores de equilibrios
+
+
+def tau_n(v):
+    return 1.0 / (alpha_n(v) + beta_n(v))
+
+
+def tau_m(v):
+    return 1.0 / (alpha_m(v) + beta_m(v))
+
+
+def tau_h(v):
+    return 1.0 / (alpha_h(v) + beta_h(v))
+
+
+def n_inf(v):
+    return alpha_n(v) * tau_n(v)
+
+
+def m_inf(v):
+    return alpha_m(v) * tau_m(v)
+
+
+def h_inf(v):
+    return alpha_h(v) * tau_h(v)
+
+
+
+
+def f(x, t, p):
+    """
+
+    x[0] = v(t): potencial de la membrana
+    x[1] = n(t): probabilidad de activacion de K (potasio)
+    x[2] = m(t): probabilidad de activacion Na (sodio)
+    x[3] = n(t): probabilidad de inactivacion Na (sodio)
+
+    t: 𝑡∼5𝑚𝑠  : tiempo  en milisegundos
+
+    p[0] = c = 1𝜇𝐹/𝑐𝑚2  : capacitancia de membrana (microF/cm **2)
+    p[1] = 𝑔¯Na = 120𝑚𝑆/𝑐𝑚2: conductancia máxima de Na
+    p[2] = 𝑔¯K = 36𝑚𝑆/𝑐𝑚2: conductancia máxima de  K
+    p[3] = 𝑔𝑙 = 0.3𝑚𝑆/𝑐𝑚2: conductancia máxima de perdida
+    p[4] = 𝑣Na=120𝑚𝑉: potencial de reversión de la corriente de Na (potencial de Nernst de Na)
+    p[5] = 𝑣K=−12𝑚𝑉: potencial de reversión de la corriente de  K  (potencial de Nernst de K)
+    p[6] = 𝑣𝑙=10.6𝑚𝑉: potencial de reversión de la corriente de perdida  (potencial de Nernst de la correinte de perdida)
+    p[7] = 𝑖(𝑡)∼10𝜇𝐴/𝑐𝑚2: corriente de entrada al tiempo 𝑡
+
+    """
+
+    v = x[0]
+    n = x[1]
+    m = x[2]
+    h = x[3]
+    c = p[0]
+    i = p[7]
+
+    return np.array(
+        [
+            (i(t) - ina(x, p) - ik(x, p) - il(x, p)) / c,  # ecuacion para dv/dt
+            alpha_n(v) * (1.0 - n) - beta_n(v) * n,  # ecuacion para dn/dt
+            alpha_m(v) * (1.0 - m) - beta_m(v) * m,  # ecuacion para dm/dt
+            alpha_h(v) * (1.0 - h) - beta_h(v) * h,  # ecuacion para dh/dt
+        ]
+    )
+
+
+# parametros para las probabilidades de activacion o inactivacion de las compuertas
+v = 0  # potencial de la membrana
+nt = 0  # probabilidad de activacion de K (potasio)
+mt = 0  # probabilidad de activacion de Na (sodio)
+ht = 0  # probabilidad de inactivacion Na (sodio)
+x = [v, nt, mt, ht]
+
+# parametros para formulas
+c = 1 # 1𝜇𝐹/𝑐𝑚2 
+gna = 120 #120𝑚𝑆/𝑐𝑚2
+gk = 36 #36𝑚𝑆/𝑐𝑚2
+gl = 0.3 #0.3𝑚𝑆/𝑐𝑚2
+vna = 120 #120𝑚𝑉
+vk = -12 #−12𝑚𝑉
+vl = 10.6 #10.6𝑚𝑉
+i = 10 #∼10𝜇𝐴/𝑐𝑚2
+p = [c, gna, gk, gl, vna, vk, vl, i]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# =============================================================================
+# 
+# 
+# 
+# 
+# x0 = 1.0  # condiciones iniciales
+# y0 = 1.0
+# z0 = 1.0
+# r0 = np.array([x0, y0, z0])  # inicio de trayectoria
+# 
+# 
+# a = 0
+# b = 300
+# h = 0.01
+# k = int((b - a) / h)
+# 
+# t, r = integrador_ode(rk4, f, r0, a, b, k, p)
+# t3 = t
+# x3, y3, z3 = r
+# =============================================================================
+
+
+plt.xlabel("$v$ [mV]")
+plt.xlim(-50, 120)
+plt.ylim(0.1, 1.1)
+v = np.linspace(-50, 120, 1000)
+
+plt.plot(v, 0*v, label="", linestyle="--", c="red")
+plt.plot(v, 0*v+1, label="", linestyle="--", c="red")
+plt.plot(v, np.vectorize(n_inf)(v), label="$n_{\\infty}$", c="orange")
+plt.plot(v, np.vectorize(m_inf)(v), label="$m_{\\infty}$", c="green")
+plt.plot(v, np.vectorize(h_inf)(v), label="$h_{\\infty}$", c="cyan")
+
+plt.show()
+
